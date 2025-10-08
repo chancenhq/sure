5c86d6e7576a039dbee111d85fc5801e876c6063
Juan José Mata <juanjo.mata@gmail.com>
2025-10-08 16:06:03 +0000

    Fix broken prod due to Sentry versions

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,10 +1,12 @@
+require_relative "./version"
+
 if ENV["SENTRY_DSN"].present?
   Sentry.init do |config|
     config.dsn = ENV["SENTRY_DSN"]
     config.environment = ENV["RAILS_ENV"]
     config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
     config.enabled_environments = %w[production]
-    config.release = "#{Rails.configuration.x.product_name}@#{Sure.version}"
+    config.release = "#{Rails.configuration.x.product_name}@#{Sure.version.to_release_tag}"
 
     # Add data like request headers and IP for users,
     # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
