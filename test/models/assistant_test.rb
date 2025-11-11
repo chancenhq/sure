dada195889750a61b345da79d08773d7ef8d4d16
Juan José Mata <juanjo.mata@gmail.com>
2025-11-11 13:16:24 +0000

    Fix failing tests (due to restricted AI/function calls)

--- test/models/assistant_test.rb
+++ test/models/assistant_test.rb
@@ -66,66 +66,6 @@ class AssistantTest < ActiveSupport::TestCase
     end
   end
 
-  test "responds with tool function calls" do
-    @assistant.expects(:get_model_provider).with("gpt-4.1").returns(@provider).once
-
-    # Only first provider call executes function
-    Assistant::Function::GetAccounts.any_instance.stubs(:call).returns("test value").once
-
-    # Call #1: Function requests
-    call1_response_chunk = provider_response_chunk(
-      id: "1",
-      model: "gpt-4.1",
-      messages: [],
-      function_requests: [
-        provider_function_request(id: "1", call_id: "1", function_name: "get_accounts", function_args: "{}")
-      ]
-    )
-
-    call1_response = provider_success_response(call1_response_chunk.data)
-
-    # Call #2: Text response (that uses function results)
-    call2_text_chunks = [
-      provider_text_chunk("Your net worth is "),
-      provider_text_chunk("$124,200")
-    ]
-
-    call2_response_chunk = provider_response_chunk(
-      id: "2",
-      model: "gpt-4.1",
-      messages: [ provider_message(id: "1", text: call2_text_chunks.join) ],
-      function_requests: []
-    )
-
-    call2_response = provider_success_response(call2_response_chunk.data)
-
-    sequence = sequence("provider_chat_response")
-
-    @provider.expects(:chat_response).with do |message, **options|
-      assert_equal @expected_session_id, options[:session_id]
-      assert_equal @expected_user_identifier, options[:user_identifier]
-      call2_text_chunks.each do |text_chunk|
-        options[:streamer].call(text_chunk)
-      end
-
-      options[:streamer].call(call2_response_chunk)
-      true
-    end.returns(call2_response).once.in_sequence(sequence)
-
-    @provider.expects(:chat_response).with do |message, **options|
-      assert_equal @expected_session_id, options[:session_id]
-      assert_equal @expected_user_identifier, options[:user_identifier]
-      options[:streamer].call(call1_response_chunk)
-      true
-    end.returns(call1_response).once.in_sequence(sequence)
-
-    assert_difference "AssistantMessage.count", 1 do
-      @assistant.respond_to(@message)
-      message = @chat.messages.ordered.where(type: "AssistantMessage").last
-      assert_equal 1, message.tool_calls.size
-    end
-  end
-
   private
     def provider_function_request(id:, call_id:, function_name:, function_args:)
       Provider::LlmConcept::ChatFunctionRequest.new(
